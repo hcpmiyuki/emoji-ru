2188742199c6cb0f2a8f3f19fb469ddcb59df134
hcpmiyuki <hcp.miyuki@gmail.com>
2021-10-13 18:14:58 +0900

    hcp834.comのリンクを追加

--- src/views/components/block/Footer.js
+++ src/views/components/block/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div css={footerStyle}>
       <ul>
-        <li>©2021 hcp834.com</li>
+        <li>©2021 <a href='https://www.hcp834.com'>hcp834.com</a></li>
         <li><a href='https://twitter.com/i_was_hcp' target='_blank' rel='noreferrer'><i className="fab fa-twitter"></i>i_was_hcp</a></li>
       </ul> 
     </div>
